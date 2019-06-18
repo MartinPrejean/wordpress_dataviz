79554405c8c5a5db9b0bd5007c0b6c9507e27faf
RomainMaillot <romainmaillot.rm@gmail.com>
2019-06-18 15:23:19 +0200

    :lipstick: Working header and footer

--- wp-content/themes/thedot_theme/footer.php
+++ wp-content/themes/thedot_theme/footer.php
@@ -1,13 +1,21 @@
         <footer id="footer">
-            <?php // SYNTAXE : wp_nav_menu( array $args = array() )
-                $args=array(
-                    'theme_location' => 'footer', // nom du slug
-                    'menu' => 'footermenu', // nom à donner cette occurence du menu
-                    'menu_class' => 'menu_footer', // class à attribuer au menu
-                    'menu_id' => 'menu_id' // id à attribuer au menu
-                );
-                wp_nav_menu($args);
-            ?>
+            <h1>The Dot</h1>
+            <nav>
+                <div class="logos">
+                <a class="logo_facebook" href="https://facebook"><?= file_get_contents(IMAGES_URL . "/logos/facebook.svg"); ?></a>
+                    <a class="logo_twitter" href="https://twitter.com"><?= file_get_contents(IMAGES_URL . "/logos/twitter.svg"); ?></a>
+                    <a class="logo_search" href="#"><?= file_get_contents(IMAGES_URL . "/logos/search.svg"); ?></a>
+                </div>
+                <?php // SYNTAXE : wp_nav_menu( array $args = array() )
+                    $args=array(
+                        'theme_location' => 'footer', // nom du slug
+                        'menu' => 'footermenu', // nom à donner cette occurence du menu
+                        'menu_class' => 'menu_footer', // class à attribuer au menu
+                        'menu_id' => 'menu_id' // id à attribuer au menu
+                    );
+                    wp_nav_menu($args);
+                ?>
+            </nav>
         </footer>
 
         <!-- Execution de la fonction wp_footer() obligatoire ! -->
