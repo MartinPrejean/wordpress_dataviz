a2f35bfec44dacac49c85709713a0f131af3efff
RomainMaillot <romainmaillot.rm@gmail.com>
2019-06-19 19:44:55 +0200

    :spakles: Adding the latest article

--- wp-content/themes/thedot_theme/front-page.php
+++ wp-content/themes/thedot_theme/front-page.php
@@ -1,6 +1,32 @@
 <?php get_header(); //appel du template header.php  ?>
     <main id="homepage-main" class="homepage-main">
         <p class="homepage-description">Nous sommes un média neutre sur l’impact économique et politique de la consommation d’énergie.</p>
+        <div class="latest-post">
+            <?php
+                // boucle WordPress
+                if (have_posts()){
+                    the_post();
+            ?>
+                <a href="<?= get_permalink(); ?>" class="latest-article">
+                    <div class="thumbnail"><?php the_post_thumbnail('single_thumbnail'); ?></div>
+                    <div class="latest-article-content">
+                        <h2 class="article-title"><?php the_title(); ?></h2>
+                        <div class="texts"><?php $content = get_the_content(); echo mb_strimwidth($content, 0, 100, '...');?></div>
+                        <div class="article-bottom">
+                            <span>By <?php the_author() ?></span>
+                            <span><?php the_time('F jS, Y') ?></span>
+                        </div>
+                    </div>
+                </a>
+            <?php
+                }
+                else {
+                    ?>
+                        Nous n'avons pas trouvé d'article répondant à votre recherche
+                    <?php
+                }
+            ?>
+        </div>
         <div class="articles-container before">
             <?php
                 // boucle WordPress
