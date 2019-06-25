92a5d76c800cf07550434c75f0899d74625f8c4d
RomainMaillot <romainmaillot.rm@gmail.com>
2019-06-26 00:04:13 +0200

    :spakles: Adding new dataviz

--- wp-content/themes/thedot_theme/inc/styles_scripts.php
+++ wp-content/themes/thedot_theme/inc/styles_scripts.php
@@ -9,16 +9,28 @@ function ajout_scripts() {
     wp_enqueue_script('glide_script');
 
     // enregistrement d'un nouveau script
-    wp_register_script('main_script', get_template_directory_uri() . '/assets/scripts/main.js', array('jquery'),'1.1', true);
-
-    // appel du script dans la page
-    wp_enqueue_script('main_script');
-
-    // enregistrement d'un nouveau script
     wp_register_script('bubbles_script', get_template_directory_uri() . '/assets/scripts/bubbles.js', array('jquery'),'1.1', true);
 
     // appel du script dans la page
     wp_enqueue_script('bubbles_script');
+
+    // enregistrement d'un nouveau script
+    wp_register_script('chart_script', get_template_directory_uri() . '/assets/scripts/Chart.bundle.js', array('jquery'),'1.1', true);
+
+    // appel du script dans la page
+    wp_enqueue_script('chart_script');
+
+    // enregistrement d'un nouveau script
+    wp_register_script('chart_min_script', get_template_directory_uri() . '/assets/scripts/Chart.bundle.min.js', array('jquery'),'1.1', true);
+
+    // appel du script dans la page
+    wp_enqueue_script('chart_min_script');
+
+    // enregistrement d'un nouveau script
+    wp_register_script('main_script', get_template_directory_uri() . '/assets/scripts/main.js', array('jquery'),'1.1', true);
+
+    // appel du script dans la page
+    wp_enqueue_script('main_script');
 
     // enregistrement d'un nouveau script
     wp_register_script('bubblesWorld_script', get_template_directory_uri() . '/assets/scripts/bubblesWorld.js', array('jquery'),'1.1', true);
--- wp-content/themes/thedot_theme/template-parts/page/content-dataviz2.php
+++ wp-content/themes/thedot_theme/template-parts/page/content-dataviz2.php
@@ -1,25 +1,8 @@
-<div id="container">
-    <div class="charts">
-        <div class="bubbleContainer">
-            <h2>La part des énergies en France</h2>
-            <div id="overallRatings" class="btn-group btn-info top-btn" data-toggle="buttons">
-                <label class="btn active ratingBtn" id="reset">
-                <input type="radio" name="options">Reset</label>
-                <label class="btn ratingBtn" id="TypeOfEnergy">
-                <input type="radio" name="options">Type d'energie</label>
-                <label class="btn ratingBtn" id="Details">
-                <input type="radio" name="options">Details type</label>
-            </div>
-            <div id="RiskCycle" class="btn-group btn-default top-btn" data-toggle="buttons">
-                <label class="btn cycleBtn" id="2005">
-                <input type="radio" name="options">2005</label>
-                <label class="btn cycleBtn" id="2010">
-                <input type="radio" name="options">2010</label>
-                <label class="btn active cycleBtn" id="2015">
-                <input type="radio" name="options">2015</label>
-            </div>
-            <div id="bubbleChart">
-            </div>
-        </div>
+<div class="canvas-container">
+    <div class="canvas">
+        <canvas id="myChart"></canvas>
+    </div>
+    <div class="canvas">
+        <canvas id="myChart2"></canvas>
     </div>
 </div>
